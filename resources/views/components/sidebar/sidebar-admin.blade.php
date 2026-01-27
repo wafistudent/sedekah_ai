4e3244f452f65ea8b9dc542034b95dfe2ea6966f
wafihafidzon <wafihafidzon@gmail.com>
2026-01-28 00:24:58 +0700

    add role spatie setting

--- resources/views/components/sidebar/sidebar-admin.blade.php
+++ resources/views/components/sidebar/sidebar-admin.blade.php
@@ -31,14 +31,14 @@
             </li>
 
             {{-- Member Routes --}}
-            <li class="{{ auth()->user()->hasRole('admin') ? 'hidden' : 'block' }}">
-                <a href="{{ route('members.network-tree') }}"
+            <li>
+                <a href="{{ route('admin.members') }}"
                     class="flex items-center rounded-lg px-3 py-2.5 text-gray-900 hover:bg-gray-100 {{ request()->routeIs('members.*') ? 'bg-gray-100' : '' }}">
                     <svg class="h-5 w-5 text-gray-500" fill="currentColor" viewBox="0 0 20 20">
                         <path
                             d="M9 6a3 3 0 11-6 0 3 3 0 016 0zM17 6a3 3 0 11-6 0 3 3 0 016 0zM12.93 17c.046-.327.07-.66.07-1a6.97 6.97 0 00-1.5-4.33A5 5 0 0119 16v1h-6.07zM6 11a5 5 0 015 5v1H1v-1a5 5 0 015-5z" />
                     </svg>
-                    <span class="ml-3">Network Tree</span>
+                    <span class="ml-3">Members</span>
                 </a>
             </li>
 
